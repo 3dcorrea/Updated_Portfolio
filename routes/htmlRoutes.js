d4ab3612ab0880e43056b6569293f5dd30a41698
Dan Correa <dancorrea3@gmail.com>
2019-06-22 14:44:28 -0400

    deleted route from htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,3 @@
-var db = require("../models");
 var path = require("path");
 
 module.exports = function (app) {
@@ -8,17 +7,6 @@ module.exports = function (app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function (req, res) {
-    db.Example.findOne({
-      where: {
-        id: req.params.id
-      }
-    }).then(function (dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
 
   // Render 404 page for any unmatched routes
   app.get("*", function (req, res) {
