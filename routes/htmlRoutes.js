21888546004b1ae25bb26556bad09fe3a4a0547a
Dan Correa <dancorrea3@gmail.com>
2019-06-22 12:44:05 -0400

    changed route in htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,8 +2,8 @@ var db = require("../models");
 
 module.exports = function (app) {
   // Load index page
-  app.get("/", function (req, res) {
-    res.render("index");
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
   });
 
   // Load example page and pass in an example by id
