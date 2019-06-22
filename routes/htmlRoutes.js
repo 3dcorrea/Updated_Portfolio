59e9eff044e6e0b68a03fbe76b010af85490f855
Dan Correa <dancorrea3@gmail.com>
2019-06-22 12:28:44 -0400

    changed route for root

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,18 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+  app.get("/", function (req, res) {
+    res.render("index")
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -16,7 +20,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
